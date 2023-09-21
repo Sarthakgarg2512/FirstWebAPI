c98ed4f19bf3dce9a4542bdb314848070263b3dd
sarthakgarg2512 <sarthak.garg@gavstech.com>
2023-09-21 18:34:04 +0530

    Employees can be seen sing post method

---
+++ FirstWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using FirstWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace FirstWebAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class EmployeeController : ControllerBase
+    {
+        private  RepositoryEmployee _repositoryEmployee;
+        public EmployeeController(RepositoryEmployee repositoryEmployee)
+        {
+            _repositoryEmployee = repositoryEmployee;
+        }
+        
+        [HttpGet]
+        //GET: EmployeeController
+        public List<Employee> GetAllEmployees()
+        {
+            List<Employee> employees = _repositoryEmployee.GetAllEmployees();
+            return employees;
+        }
+        [HttpPost]
+        public Employee EmployeeDetails(int id)
+        {
+            Employee employees = _repositoryEmployee.GetEmployee(id);
+            return employees;
+        }
+    }
+}
+
---
+++ FirstWebAPI/Models/RepositoryEmployee.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FirstWebAPI.Models
+{
+    public class RepositoryEmployee
+    {
+        private  NorthwindContext _context;
+        public RepositoryEmployee(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+        public Employee GetEmployee(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            return employee;
+        }
+    }
+}

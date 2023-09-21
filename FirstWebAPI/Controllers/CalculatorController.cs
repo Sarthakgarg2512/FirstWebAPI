a4cbaa9aed3e8d7db96f8809bbb2b858134f1d61
Sarthak Garg <Sarthak.garg@gavstech.com>
2023-09-21 12:05:25 +0530

    First Web API Commit

---
+++ FirstWebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/Calculator/Add?x=2&y=10
+        [HttpGet("Calculator/add")]
+        public int Add(int x, int y)
+        { 
+            return x+y; 
+        } 
+        [HttpGet("Calculator/sum")]
+        public int Sum(int x, int y)
+        { 
+            return x+y+1000; 
+        }
+        //api/calculator/Subtract? x = 20 & y = 10
+       [HttpPost]
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        // api/calculator/Multiply?x=250&y=10
+        [HttpPut]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        // api/calculator/Divide?x=200&y=10
+        [HttpDelete]
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
